7b856131a92857a9057ddbce4a374a8e8640daad
lonelypenguin245 <144401532+lonelypenguin245@users.noreply.github.com>
2023-09-08 08:51:38 +0700

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LinearSearch;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HI
+ */
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of array:");
+        int length = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter search value:");
+        int search = scanner.nextInt();
+
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = new Random().nextInt(length);
+        }
+        LinearSearch searcher = new LinearSearch();
+        System.out.print("The array: ");
+        searcher.displayArray(array);
+
+        int foundIndex = searcher.linerSearch(array, search);
+        System.out.println("\nFound " + search + " at index: " + foundIndex);
+    }
+
+    public void displayArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+
+    public int linerSearch(int[] arr, int key) {
+
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
